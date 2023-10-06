7c3aa911b6749693a64a2e284f69d6834a627605
Tiit Ülejõe <tiit@infokiir.ee>
2023-10-06 11:09:25 +0300

    Initial commit

---
+++ test/constexpr_tests.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <myproject/sample_library.hpp>
+
+
+TEST_CASE("Factorials are computed with constexpr", "[factorial]")
+{
+	  STATIC_REQUIRE(factorial_constexpr(0) == 1); // cppcheck-suppress knownConditionTrueFalse
+	  STATIC_REQUIRE(factorial_constexpr(1) == 1); // cppcheck-suppress knownConditionTrueFalse
+	  STATIC_REQUIRE(factorial_constexpr(2) == 2); // cppcheck-suppress knownConditionTrueFalse
+	  STATIC_REQUIRE(factorial_constexpr(3) == 6); // cppcheck-suppress knownConditionTrueFalse
+	  STATIC_REQUIRE(factorial_constexpr(10) == 3628800); // cppcheck-suppress knownConditionTrueFalse
+}
